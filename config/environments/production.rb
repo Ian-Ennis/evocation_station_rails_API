700b46730cf3496f843857d18c978190e1150a94
Ian <ipatrickenn7@gmail.com>
2022-05-31 12:17:19 -0600

    local storage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   # Switching to cloudinary file storage system:
-  config.active_storage.service = :cloudinary
+  config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
