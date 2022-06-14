f07223ee26df5c3ebb0d897a3a85fecd642c0229
Ian <ipatrickenn7@gmail.com>
2022-06-14 12:58:13 -0600

    removed writing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
 User.destroy_all
 
 Newevocation.destroy_all
@@ -24,9 +16,7 @@ Writing.create(text: "Awe")
 Writing.create(text: "Beaming")
 Writing.create(text: "Beauty")
 Writing.create(text: "Backbone")
-Writing.create(text: "Baleful")
 Writing.create(text: "Bellicose")
-Writing.create(text: "Bereaved")
 Writing.create(text: "Bewildered")
 Writing.create(text: "Bleak")
 Writing.create(text: "Blinding")
@@ -35,12 +25,8 @@ Writing.create(text: "Brazen")
 Writing.create(text: "Breath")
 Writing.create(text: "Broken")
 Writing.create(text: "Captivated")
-Writing.create(text: "Cashmere")
-Writing.create(text: "Caustic")
 Writing.create(text: "Chaotic")
 Writing.create(text: "Cheerful")
-Writing.create(text: "Chipper")
-Writing.create(text: "Clench")
 Writing.create(text: "Coarse")
 Writing.create(text: "Content")
 Writing.create(text: "Corpulent")
@@ -50,18 +36,12 @@ Writing.create(text: "Crippling")
 Writing.create(text: "Crisp")
 Writing.create(text: "Curious")
 Writing.create(text: "Deafening")
-Writing.create(text: "Decorous")
 Writing.create(text: "Defy")
 Writing.create(text: "Delighted")
 Writing.create(text: "Demagogue")
-Writing.create(text: "Desire")
-Writing.create(text: "Disastrous")
-Writing.create(text: "Dowdy")
-Writing.create(text: "Dulcet")
 Writing.create(text: "Dynamic")
 Writing.create(text: "Eccentric")
 Writing.create(text: "Elated")
-Writing.create(text: "Emollient")
 Writing.create(text: "Energetic")
 Writing.create(text: "Entertained")
 Writing.create(text: "Envy")
@@ -70,24 +50,16 @@ Writing.create(text: "Euphoric")
 Writing.create(text: "Excited")
 Writing.create(text: "Exhilirated")
 Writing.create(text: "Exude")
-Writing.create(text: "Fallen")
 Writing.create(text: "Fantastic")
-Writing.create(text: "Feckless")
-Writing.create(text: "Feisty")
 Writing.create(text: "Fickle")
-Writing.create(text: "Florid")
-Writing.create(text: "Forbidden")
 Writing.create(text: "Frenetic")
-Writing.create(text: "Frown")
 Writing.create(text: "Giddy")
 Writing.create(text: "Gleeful")
 Writing.create(text: "Gloom")
 Writing.create(text: "Gratified")
-Writing.create(text: "Groan")
 Writing.create(text: "Haggard")
 Writing.create(text: "Happy")
 Writing.create(text: "Havoc")
-Writing.create(text: "Hiss")
 Writing.create(text: "Hopeful")
 Writing.create(text: "Hunger")
 Writing.create(text: "Incendiary")
@@ -104,7 +76,6 @@ Writing.create(text: "Lush")
 Writing.create(text: "Merry")
 Writing.create(text: "Murmur")
 Writing.create(text: "Need")
-Writing.create(text: "Oath")
 Writing.create(text: "Off-limits")
 Writing.create(text: "Optimistic")
 Writing.create(text: "Opulent")
@@ -113,7 +84,6 @@ Writing.create(text: "Peaceful")
 Writing.create(text: "Pervasive")
 Writing.create(text: "Playful")
 Writing.create(text: "Pleasant")
-Writing.create(text: "Pliant")
 Writing.create(text: "Pluck")
 Writing.create(text: "Plunge")
 Writing.create(text: "Positive")
@@ -122,20 +92,14 @@ Writing.create(text: "Powerful")
 Writing.create(text: "Pungent")
 Writing.create(text: "Rancorous")
 Writing.create(text: "Raspy")
-Writing.create(text: "Recalcitrant")
-Writing.create(text: "Rejection")
 Writing.create(text: "Rejoice")
-Writing.create(text: "Restrained")
 Writing.create(text: "Rigid")
-Writing.create(text: "Ruthless")
 Writing.create(text: "Scheme")
-Writing.create(text: "Searing")
 Writing.create(text: "Secret")
 Writing.create(text: "Shatter")
 Writing.create(text: "Sheer")
 Writing.create(text: "Shrill")
 Writing.create(text: "Shuffled")
-Writing.create(text: "Sinful")
 Writing.create(text: "Skittish")
 Writing.create(text: "Smiling")
 Writing.create(text: "Solid")
@@ -143,84 +107,67 @@ Writing.create(text: "Sparkling")
 Writing.create(text: "Spunk")
 Writing.create(text: "Stark")
 Writing.create(text: "Startle")
-Writing.create(text: "Stubby")
 Writing.create(text: "Sunny")
-Writing.create(text: "Surreptitious")
 Writing.create(text: "Thankful")
 Writing.create(text: "Thermal")
-Writing.create(text: "Thick")
 Writing.create(text: "Thrilled")
 Writing.create(text: "Thundering")
 Writing.create(text: "Turbulent")
 Writing.create(text: "Twisted")
 Writing.create(text: "Upbeat")
-Writing.create(text: "Venomous")
 Writing.create(text: "Visceral")
-Writing.create(text: "Voracious")
-Writing.create(text: "Vulnerable")
-Writing.create(text: "Watery")
 Writing.create(text: "Weathered")
 Writing.create(text: "Whisper")
 
-Writing.create(text: "Two roads diverged in a yellow wood,
-    And sorry I could not travel both
-    And be one traveler, long I stood
-    And looked down one as far as I could
-    To where it bent in the undergrowth;
+# Writing.create(text: "Two roads diverged in a yellow wood,
+#     And sorry I could not travel both
+#     And be one traveler, long I stood
+#     And looked down one as far as I could
+#     To where it bent in the undergrowth;
     
-    Then took the other, as just as fair,
-    And having perhaps the better claim,
-    Because it was grassy and wanted wear;
-    Though as for that the passing there
-    Had worn them really about the same,
+#     Then took the other, as just as fair,
+#     And having perhaps the better claim,
+#     Because it was grassy and wanted wear;
+#     Though as for that the passing there
+#     Had worn them really about the same,
     
-    And both that morning equally lay
-    In leaves no step had trodden black.
-    Oh, I kept the first for another day!
-    Yet knowing how way leads on to way,
-    I doubted if I should ever come back.
+#     And both that morning equally lay
+#     In leaves no step had trodden black.
+#     Oh, I kept the first for another day!
+#     Yet knowing how way leads on to way,
+#     I doubted if I should ever come back.
     
-    I shall be telling this with a sigh
-    Somewhere ages and ages hence:
-    Two roads diverged in a wood, and I—
-    I took the one less traveled by,
-    And that has made all the difference.")
+#     I shall be telling this with a sigh
+#     Somewhere ages and ages hence:
+#     Two roads diverged in a wood, and I—
+#     I took the one less traveled by,
+#     And that has made all the difference.")
 
-Writing.create(text: "We real cool. We
-    Left school. We
+# Writing.create(text: "We real cool. We
+#     Left school. We
     
-    Lurk late. We
-    Strike straight. We
+#     Lurk late. We
+#     Strike straight. We
     
-    Sing sin. We
-    Thin gin. We
+#     Sing sin. We
+#     Thin gin. We
     
-    Jazz June. We
-    Die soon.")
+#     Jazz June. We
+#     Die soon.")
 
-Writing.create(text: "What happens to a dream deferred?
+# Writing.create(text: "What happens to a dream deferred?
 
-    Does it dry up
-    like a raisin in the sun?
-    Or fester like a sore—
-    And then run?
-    Does it stink like rotten meat?
-    Or crust and sugar over—
-    like a syrupy sweet?
+#     Does it dry up
+#     like a raisin in the sun?
+#     Or fester like a sore—
+#     And then run?
+#     Does it stink like rotten meat?
+#     Or crust and sugar over—
+#     like a syrupy sweet?
 
-    Maybe it just sags
-    like a heavy load.
+#     Maybe it just sags
+#     like a heavy load.
 
-    Or does it explode?")
+#     Or does it explode?")
 
 puts "Writings seeded"
-
-
-# puts "Seeding images..."
-# Image.create!(audio: "")
-# puts "Images seeded"
-
-# puts "Seeding evocations.."
-# Evocation.create!(text: "first evocation", fi)
-# Evocation.create!(text: "second evocation")
-# puts "Done seeding"
