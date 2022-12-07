7bbebbfbcb83b0a559e082bbff0a48f94e0e8c6c
Ian <ipatrickenn7@gmail.com>
2022-12-07 14:08:08 -0700

    Authorization fixed, models associated with current user work

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   skip_before_action :require_login, only: [:create]
 
-  def profile
-    render json: { user: current_user }, status: :accepted
-  end
-
   def create
     @user = User.create!(user_params)
     
--- app/controllers/newevocations_controller.rb
+++ app/controllers/newevocations_controller.rb
@@ -1,15 +1,13 @@
 class NewevocationsController < ApplicationController
-    # skip_before_action :require_login, only: [:index, :create]
 
     def index
-        puts "we're in index"
         @user = current_user
         @newevocations = @user.newevocations
         render json: @newevocations
     end
 
-    def create 
-        evocation = Newevocation.create!(user_id: params[:user_id], text: params[:text], image: params[:image], audio: params[:audio])
+    def create
+        evocation = Newevocation.create!(evocation_params)
         if evocation.valid?
             render json: evocation, status: :created
         else 
@@ -17,16 +15,16 @@ class NewevocationsController < ApplicationController
         end
     end
 
-    # def destroy
-    #     evocation = Newevocation.find(params[:id])
-    #     evocation.destroy
-    #     head :no_content
-    # end
+    def destroy
+        evocation = Newevocation.find(params[:id])
+        evocation.destroy
+        head :no_content
+    end
 
-    # private
+    private
 
-    # def evocation_params
-    #     params.permit(:user_id, :id, :text, :image, :audio)
-    # end
+    def evocation_params
+        params.permit(:user_id, :id, :text, :image, :audio)
+    end
     
 end
